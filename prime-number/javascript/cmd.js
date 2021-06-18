818bd3994fc981a982913d03603b5feebf194ac9
Michael Afanasyev <zyxd@syncraft.ru>
2021-06-18 16:48:07 +0400

    Some JS optimizations

--- prime-number/javascript/cmd.js
+++ prime-number/javascript/cmd.js
@@ -1,19 +1,22 @@
-let primeNumberCount = process.argv[2] || 100;
-let number = 0;
-
-while (primeNumberCount > 0) {
-    number++;
-    let j = 0;
-
-    for (let i = 1; i <= number; i++) {
-        if (number % i === 0) {
-            j++;
-        }
+const isPrime = number => {
+  for (let i = 2; i < number; i++) {
+    if (number % i === 0) {
+      return false
     }
+  }
 
-    if (j === 2) {
-        primeNumberCount--;
-    }
+  return number > 1
 }
 
-console.log('The latest prime number: ' + number);
+let primeNumberCount = process.argv[2] || 100
+let number = 0
+
+while (primeNumberCount > 0) {
+  number++
+
+  if (isPrime(number)) {
+    primeNumberCount--
+  }
+}
+
+console.log('The latest prime number: ' + number)
