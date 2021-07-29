9cca7e0eebc06a20f22d2e860d761081d9cb43ec
Dmirty Mamchyts <mamchyts@gmail.com>
2021-07-29 15:30:17 +0300

    Create cmd.ts

---
+++ prime-number/typescript/cmd.ts
@@ -0,0 +1,19 @@
+let primeNumberCount: number = parseInt(process.argv[2]) || 100;
+let number:number = 0;
+
+while (primeNumberCount > 0) {
+    number++;
+    let j = 0;
+
+    for (let i = 1; i <= number; i++) {
+        if (number % i === 0) {
+            j++;
+        }
+    }
+
+    if (j === 2) {
+        primeNumberCount--;
+    }
+}
+
+console.log('The latest prime number: ' + number);
