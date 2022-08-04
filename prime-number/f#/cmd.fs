c6281ac0c5cd859ed32b87c6120e4303f4871753
Dmitry Mamchyts <dmitry.mamchyts@hitapps.com>
2022-08-04 10:51:10 +0300

    Add F#

---
+++ prime-number/f#/cmd.fs
@@ -0,0 +1,18 @@
+﻿[<EntryPoint>]
+let main args =
+    let mutable primeNumberCount : int32 = if (args.Length > 0) then int args[0] else 100
+
+    let mutable number : int32 = 0
+    while (primeNumberCount > 0) do
+        number <- number + 1
+        let mutable j : int32 = 0
+
+        for i = 1 to number do
+            if (number % i = 0) then
+                j <- j + 1
+
+        if (j = 2) then
+            primeNumberCount <- primeNumberCount - 1
+
+    printfn "The latest prime number: %d\n" number
+    0
